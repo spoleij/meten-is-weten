ed79ed55758a9e50db7be7cf7737fde42300bc23
Steph Poleij <99033734@mydavinci.nl>
2021-09-10 11:19:50 +0200

    Min en Max

--- getallenVergelijken.py
+++ getallenVergelijken.py
@@ -3,9 +3,16 @@ getalB = input ('Geef nu nog een willekeurig getal B:  ')
 
 if getalA > getalB:
     Max = getalA
-    print ('A is het grootste getal:  ' + str(Max))
+    Min = getalB
+    print ('A is het grootste getal:  ' + str(Max)) 
+    print ('het minimum is ' + str(Min) +' het maximum is ' + str(Max))
 elif getalA < getalB:
     Min = getalA
+    Max = getalB
     print ('A is het kleinste getal:  ' + str(Min))
+    print ('het minimum is ' + str(Min) +' het maximum is ' + str(Max))
 else:
+    Min = getalA
+    Max = getalB
     print ('A en B zijn even groot')
+    print ('het minimum is ' + str(Min) +' en het maximum is ' + str(Max))
