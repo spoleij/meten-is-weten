4f9f9d6cc16c4ea6d6ea82b7853d65c4b7e1a611
Steph Poleij <99033734@mydavinci.nl>
2021-09-10 09:54:17 +0200

    input en if-statement

---
+++ getallenVergelijken.py
@@ -0,0 +1,6 @@
+getalA = input('Geef een willekeurig getal:  ')
+getalB = input ('Geef nu nog een willekeurig getal:  ')
+
+if getalA > getalB:
+    Max = getalA
+    print ('a is het grootste getal:  ' + str(Max))
